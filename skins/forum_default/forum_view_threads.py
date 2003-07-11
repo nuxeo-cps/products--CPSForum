360f3c6d19393e26410ba5700d94acd2832195ec
dwyart <devnull@localhost>
2003-07-11 14:12:36 +0000

    Rajout d'un script pour appel plus propre

---
+++ skins/forum_default/forum_view_threads.py
@@ -0,0 +1,8 @@
+##parameters=post_id, threads=None, is_reviewer=None
+
+doc = context.getContent();
+result = []
+for i in doc.getThreads():
+    result.append((i, doc.getDescendants(i['id']),))
+
+return context.forum_view_threads_main(post_id=post_id, descendants=result, is_reviewer=is_reviewer)

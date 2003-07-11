e633edd5f8ef5dcf6c5cc46e88ed8ec85fe8fbb6
dwyart <devnull@localhost>
2003-07-11 15:03:33 +0000

    Typo

--- skins/forum_default/forum_post_reply.py
+++ skins/forum_default/forum_post_reply.py
@@ -1,6 +1,6 @@
 ##parameters=error_message=''
 
 parent_id=context.REQUEST.get('parent_id', None)
-return context.forum_post_form_main(error_message=error_message, \
+return context.forum_post_form(error_message=error_message, \
     parent_id=parent_id)
 

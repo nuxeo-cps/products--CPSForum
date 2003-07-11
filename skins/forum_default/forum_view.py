5cdf087850fe4a7a20f522c7a64e2d8f20aee1c7
dwyart <devnull@localhost>
2003-07-11 13:58:21 +0000

    Debug, debug, debug...

--- skins/forum_default/forum_view.py
+++ skins/forum_default/forum_view.py
@@ -2,11 +2,9 @@
 ##parameters=post_id=None, REQUEST=None
 """ """
 
-if(post_id is None):
+if post_id is None:
     threads = context.getContent().getThreads()
     if(len(threads)>0):
-        context.forum_view_main(post_id=threads[0]['id'])
-    else:
-        context.forum_view_main()
-else:
-    context.forum_view_main()
+        return context.forum_view_main(post_id=threads[0]['id'])
+
+return context.forum_view_main()

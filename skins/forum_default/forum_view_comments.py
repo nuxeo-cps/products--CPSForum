16a31bdcee5adb84e04c73df3d91391b3e179b08
epietrig <devnull@localhost>
2003-10-09 16:10:41 +0000

    similar to forum_view_threads, but with some modifications for use in document comments mode

---
+++ skins/forum_default/forum_view_comments.py
@@ -0,0 +1,9 @@
+##parameters=post_id, forum
+
+result = []
+for i in forum.getThreads():
+    result.append((i, forum.getDescendants(i['id']),))
+
+return context.forum_view_threads_main(post_id=post_id, descendants=result,
+                                       is_reviewer=0, comment_mode=1,
+                                       forum=forum)

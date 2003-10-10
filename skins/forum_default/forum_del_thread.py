ab5b481f3c4b621d8e0be89053f1e001581924ff
epietrig <devnull@localhost>
2003-10-10 11:07:28 +0000

    reusing existing code

--- skins/forum_default/forum_del_thread.py
+++ skins/forum_default/forum_del_thread.py
@@ -1,9 +1,7 @@
 ##parameters=parent_id,comment_mode
 
-if comment_mode:
-    doc = context.portal_comment.getDiscussionFor(context)
-else:
-    doc = hasattr(context, 'getContent') and context.getContent() or context
+doc = context.getForumObject(comment_mode)
+
 doc.delForumPost(parent_id)
 
 context.REQUEST.RESPONSE.redirect(context.absolute_url() + "/view")

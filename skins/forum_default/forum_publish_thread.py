fd4f05874ce95403f91a1fb94a4acbf6f727100f
sfermigier <devnull@localhost>
2003-12-13 16:24:48 +0000

    Rename skin methods.

--- skins/forum_default/forum_publish_thread.py
+++
@@ -1,9 +0,0 @@
-##parameters=parent_id, REQUEST
-
-# $Id$
-
-doc = hasattr(context, 'getContent') and context.getContent() or context
-doc.publishPost(parent_id)
-REQUEST.RESPONSE.redirect(context.absolute_url()+"/?post_id="+REQUEST.parent_id)
-
-
---
+++ skins/forum_default/forum_unpublish_posts.py
@@ -0,0 +1,11 @@
+##parameters=forum_thread_ids=(), REQUEST=None
+
+# $Id$
+
+forum = hasattr(context, 'getContent') and context.getContent() or context
+for i in forum_thread_ids:
+    forum.publishPost(i, 0)
+
+if REQUEST:
+    REQUEST.RESPONSE.redirect(
+        context.absolute_url() + "/?post_id=" + REQUEST.parent_id)

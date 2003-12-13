a6054da23fe1baf39738b0b6f16ab661d5a7ad42
sfermigier <devnull@localhost>
2003-12-13 15:41:48 +0000

    A forum is not a 'doc'.

--- skins/forum_default/forum_post.py
+++ skins/forum_default/forum_post.py
@@ -2,12 +2,12 @@
 
 # $Id$
 
-doc = context.getForumObject(comment_mode)
+forum = context.getForumObject(comment_mode)
 
-if (not doc) and comment_mode:
+if (not forum) and comment_mode:
     #the forum holding comments might not exist if this is
     #the first post ; in this case create it
-    doc = context.portal_discussion.addForum(context)
+    forum = context.portal_discussion.addForum(context)
 
 if (not author) or (author == ""):
     # XXX: this message must be translated
@@ -22,8 +22,8 @@ if (not parent_id) or parent_id.isspace():
     #of None when going through "New Thread"
     parent_id = None
 
-new_id = doc.addForumPost(subject=subject, author=author,
-                          message=message, parent_id=parent_id)
+new_id = forum.addForumPost(subject=subject, author=author,
+                            message=message, parent_id=parent_id)
 
 if REQUEST:
     if comment_mode:

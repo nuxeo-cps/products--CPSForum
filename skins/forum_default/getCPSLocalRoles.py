638f962469392b069434c4772414bc3e712228de
janguenot <devnull@localhost>
2004-01-05 19:30:46 +0000

    Using the mergedLocalRolesWithPath

--- skins/forum_default/getCPSLocalRoles.py
+++ skins/forum_default/getCPSLocalRoles.py
@@ -1,8 +1,6 @@
-## Script (Python) "getCPSLocalRoles.py"
 ##parameters=mtool=None, base_url=None, context_url=None
 ##
 # $Id$
-
 """ get Merged Local Roles filtering non CPS Roles. """
 
 if mtool is None:
@@ -15,13 +13,14 @@ if context_url is None:
     context_url = context.getContextUrl()
 
 # Get the list of Roles from the tool
-dict_roles = mtool.getMergedLocalRoles(context, withpath=1)
+dict_roles = mtool.getMergedLocalRolesWithPath(context)
 
 # Filter remove non CPS roles
 for user in dict_roles.keys():
     for item in dict_roles[user]:
-        item['roles'] = [x for x in item['roles'] 
-                           if x not in ('Owner', 'Member')]
+        item['roles'] = [x for x in item['roles'] if x not in ('Owner',
+                                                               'Member'
+                                                               )]
     dict_roles[user] = [x for x in dict_roles[user] if len(x['roles'])]
 
     if not len(dict_roles[user]):
@@ -31,15 +30,12 @@ for user in dict_roles.keys():
 editable_users = []
 for user in dict_roles.keys():
     for item in dict_roles[user]:
-        if base_url + item['url'] == context_url:
+        if base_url+item['url'] == context_url:
             editable_users.append(user)
             continue
 
 # List local roles according to the context
-cps_roles = mtool.getCPSCandidateLocalRoles(context)
-
-
-from zLOG import LOG, DEBUG
+cps_roles = mtool.getCPSCandidateLocalRoles( context )
 
 # Filter them for CPS
 cps_roles = [x for x in cps_roles if x not in ('Owner',
@@ -47,23 +43,15 @@ cps_roles = [x for x in cps_roles if x not in ('Owner',
                                                'Reviewer',
                                                'Manager',
                                                'Authenticated')]
-
-# Checking the context (Ws or section or forum)
+# Checking the context (Ws or section)
 if context.portal_type == "Section":
     cps_roles = [x for x in cps_roles if x not in ('WorkspaceManager',
                                                    'WorkspaceMember',
-                                                   'WorkspaceReader',
-                                                   'ForumPoster',
-                                                   'ForumModerator')]
+                                                   'WorkspaceReader')]
 elif context.portal_type == "Workspace":
     cps_roles = [x for x in cps_roles if x not in ('SectionManager',
                                                    'SectionReader',
-                                                   'SectionReviewer',
-                                                   'ForumPoster',
-                                                   'ForumModerator')]
-elif context.portal_type == "CPSForum":
-    cps_roles = [x for x in cps_roles if x in ('ForumPoster',
-                                               'ForumModerator')]
+                                                   'SectionReviewer')]
 else:
     cps_roles = cps_roles
 

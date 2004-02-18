6242c2f05186290b18e74a95b62a47084d25e176
epietrig <devnull@localhost>
2004-02-18 08:24:07 +0000

    deal with the case of forums used as commenting tool for cpsdocument

--- skins/forum_default/isAllowedToPost.py
+++ skins/forum_default/isAllowedToPost.py
@@ -1,14 +1,15 @@
-##parameters=is_poster=0,is_anon=1
+##parameters=is_poster=0,is_anon=1,forum=None
 # $Id$
 
 """Determine whether the current user has posting rights on a forum"""
 
-forum = context.getContent()
-
-if forum.anonymousPostsAllowed():
-    return 1
-else:
-    if not is_anon and is_poster:
+if forum:
+    if forum.anonymousPostsAllowed():
         return 1
     else:
-        return 0
+        if not is_anon and is_poster:
+            return 1
+        else:
+            return 0
+else:
+    return 0

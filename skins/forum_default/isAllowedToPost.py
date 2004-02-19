5c7de914ff86bbb66b60cc9e7c6885f12fc5291a
epietrig <devnull@localhost>
2004-02-19 11:07:52 +0000

    check that forum is not locked (frozen)

--- skins/forum_default/isAllowedToPost.py
+++ skins/forum_default/isAllowedToPost.py
@@ -4,12 +4,15 @@
 """Determine whether the current user has posting rights on a forum"""
 
 if forum:
-    if forum.anonymousPostsAllowed():
-        return 1
-    else:
-        if not is_anon and is_poster:
+    if not forum.isFrozen():
+        if forum.anonymousPostsAllowed():
             return 1
         else:
-            return 0
+            if not is_anon and is_poster:
+                return 1
+            else:
+                return 0
+    else:
+        return 0
 else:
     return 0

0db0a7fe1e7673ac649427a1ea395fb0af7fe54e
epietrig <devnull@localhost>
2004-05-11 10:22:32 +0000

    take b_start into account (batching) so that tree (un)folding redirects to appropriate page

--- skins/forum_default/forum_branch_set.py
+++ skins/forum_default/forum_branch_set.py
@@ -1,8 +1,12 @@
-##parameters=post_id, action
+##parameters=post_id, action, REQUEST=None
 
 # $Id$
+
+from ZTUtils import make_query
 
 session_data = context.session_data_manager.getSessionData()
 session_data.set('post_' + str(post_id), action)
 
-return context.forum_view_main()
+if REQUEST is not None:
+    url = "%s?%s" % (context.absolute_url(), make_query(REQUEST.form))
+    REQUEST.RESPONSE.redirect(url)

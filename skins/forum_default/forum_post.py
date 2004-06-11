7853bd3c9c9ef182475ee339a91b3fd445c93b4c
epietrig <devnull@localhost>
2004-06-11 11:42:05 +0000

    anonymous posts are handled in portal_discussion (for security reasons)

--- skins/forum_default/forum_post.py
+++ skins/forum_default/forum_post.py
@@ -14,22 +14,40 @@ if not parent_id or parent_id.isspace():
     parent_id = None
 
 post_id = context.computeId(compute_from=subject)
-context.portal_workflow.invokeFactoryFor(context, 'ForumPost', post_id,
-                                         subject=subject,
-                                         author=author,
-                                         message=message,
-                                         parent_id=parent_id)
 
-forum = context.getContent()
-forum.newPostCreated(post_id, proxy=context)
+isAnon = context.portal_membership.isAnonymousUser()
+
+if isAnon:
+    context.portal_discussion.createAnonymousForumPost(context, post_id,
+                                                  subject,
+                                                  author,
+                                                  message,
+                                                  parent_id)
+else:
+    context.portal_workflow.invokeFactoryFor(context, 'ForumPost', post_id,
+                                             subject=subject,
+                                             author=author,
+                                             message=message,
+                                             parent_id=parent_id)
+    forum = context.getContent()
+    forum.newPostCreated(post_id, proxy=context)
 
 if REQUEST is not None:
-    if frm_start:
-        url = "%s?post_id=%s&frm_start=%s" % (context.absolute_url(),
+    if not isAnon:
+        if frm_start:
+            url = "%s?post_id=%s&frm_start=%s" % (context.absolute_url(),
                                               post_id, frm_start)
+        else:
+            url = "%s?post_id=%s" % (context.absolute_url(),
+                                     post_id)
     else:
-        url = "%s?post_id=%s" % (context.absolute_url(),
-                                 post_id)
+        if frm_start:
+            url = "%s?frm_start=%s?portal_status_message=%s" % (context.absolute_url(),
+                                                                frm_start,
+                                                                'forum_psm_message_posted')
+        else:
+            url = "%s?portal_status_message=%s" % (context.absolute_url(),
+                                                   'forum_psm_message_posted')
     REQUEST.RESPONSE.redirect(url)
 else:
     return post_id

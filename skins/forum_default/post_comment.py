d40c68c759f98da7ba9547f24fb93ae2e766842a
edegoute <devnull@localhost>
2004-06-30 09:37:13 +0000

    Add an explicit message when you not have rights to post a comment

--- skins/forum_default/post_comment.py
+++ skins/forum_default/post_comment.py
@@ -11,3 +11,7 @@ if context.portal_membership.checkPermission('Forum Post', context):
     if REQUEST is not None and forum is not None:
         REQUEST.RESPONSE.redirect('%s/%s' %
                                   (forum.absolute_url(), 'forum_post_form'))
+else:
+    if REQUEST is not None:
+        REQUEST.RESPONSE.redirect('%s?portal_status_message=%s' %
+                                  (context.absolute_url(), 'psm_cpsforum_need_rights'))

1707e487ecf8dacbe64d71bba3c4a73530a8d9b6
epietrig <devnull@localhost>
2004-07-06 07:17:23 +0000

    prepare a flat list of posts for cases where posts will not be displayed in a tree (sort by author, date, subject)

--- skins/forum_default/forum_view_threads.py
+++ skins/forum_default/forum_view_threads.py
@@ -3,10 +3,22 @@
 # $Id$
 
 forum = context.getContent()
-result = []
-for root_post in forum.getThreads(proxy=context):
-    result.append((root_post, forum.getDescendants(root_post['id'],
-                                                   proxy=context)))
 
+try:
+    session_data = context.session_data_manager.getSessionData()
+    sort_by = session_data.get('frm_sort', None)
+except AttributeError:
+    sort_by = None
+
+if sort_by is None or sort_by == 'threads':
+    result = []
+    for root_post in forum.getThreads(proxy=context):
+        result.append((root_post, forum.getDescendants(root_post['id'],
+                                                       proxy=context)))
+else:
+    post_proxies = context.objectValues(['CPS Proxy Document'])
+    result = [forum.getPostInfo(proxy) for proxy in post_proxies]
+    
 return context.forum_view_threads_main(post_id=post_id, descendants=result,
                                        is_reviewer=is_reviewer, forum=forum)
+

34ad90c9d379b5c5c9897c908f56d2c8cbef6bdf
epietrig <devnull@localhost>
2004-07-06 12:14:37 +0000

    simplified code

--- skins/forum_default/forum_unpublish_posts.py
+++ skins/forum_default/forum_unpublish_posts.py
@@ -6,9 +6,7 @@ for post_id in forum_thread_ids:
     post = getattr(context, post_id)
     review_state = context.portal_workflow.getInfoFor(post, 'review_state', 'nostate')
     if review_state == 'published':
-        review_state = context.portal_workflow.getInfoFor(post, 'review_state', 'nostate')
         context.portal_workflow.doActionFor(post, 'unpublish')
-        review_state = context.portal_workflow.getInfoFor(post, 'review_state', 'nostate')
 
 if REQUEST:
     if frm_start:

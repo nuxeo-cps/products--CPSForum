9dccdc1ef30252c115bb91bb0918b5dfdb04a922
epietrig <devnull@localhost>
2004-09-06 14:39:03 +0000

    vocab for choosing wf status display mode

--- skins/forum_default/getCPSForumVocabularies.py
+++ skins/forum_default/getCPSForumVocabularies.py
@@ -13,6 +13,12 @@ vocabularies = {
             ('200fc', '200FirstChars', 'forum_label_display_200_fc'),
             ('msg', 'Message', 'forum_label_display_msg')),},
         },
+    'forum_wf_display_voc': {
+        'type': 'CPS Vocabulary',
+        'data': {'tuples': (
+            ('wf_icon', 'Icons', 'forum_label_display_wf_icon'),
+            ('wf_txt', 'Text (additional column)', 'forum_label_display_wf_txt'))},
+        },
     }
 
 return vocabularies

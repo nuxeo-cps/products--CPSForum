7284b8fd9845d1bdd9e82189edf075189c7f2871
Georges Racinet on purity.racinet.fr <georges@racinet.fr>
2011-02-07 12:41:23 +0100

    Robustness in case user hasn't enough rights

--- skins/forum_default/hasCommentAction.py
+++ skins/forum_default/hasCommentAction.py
@@ -11,8 +11,11 @@ res = False
 
 if (proxy_type in proxy_types and
     getattr(context.getContent(), 'allow_discussion', None) == 1):
-    forum = context.getForum4Comments().getContent()
     pmtool = getToolByName(context, 'portal_membership')
+    forumproxy = context.getForum4Comments()
+    if not pmtool.checkPermission('View', forumproxy):
+        return False
+    forum = forumproxy.getContent()
     is_anon = pmtool.isAnonymousUser()
     is_poster = pmtool.checkPermission('Forum Post', forum)
     res = context.isAllowedToPost(is_poster=is_poster,
